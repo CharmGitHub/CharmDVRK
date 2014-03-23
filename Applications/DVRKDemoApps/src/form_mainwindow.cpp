5229e7583c06068741ea3eccc6c86ac97633ac1e
CharmAdmin <charmlabdropbox@gmail.com>
2014-03-23 13:08:23 -0700

    Put in more functionality to DVRKDemoApps

--- Applications/DVRKDemoApps/src/form_mainwindow.cpp
+++ Applications/DVRKDemoApps/src/form_mainwindow.cpp
@@ -11,7 +11,22 @@ Form_MainWindow::Form_MainWindow(QWidget *parent) :
 //===================================================================
 Form_MainWindow::~Form_MainWindow()
 {
+    // Set the display thread end to true
+    p_sharedData->m_flagDisplayThreadEnded = true;
+
+    // Wait for other threads to end
+    while((p_sharedData->m_flagControlThreadEnd == false) || (p_sharedData->m_flagStateMachineThreadEnd == false))
+    {
+
+    }
+
     delete ui;
+}
+
+//===================================================================
+void Form_MainWindow::Init()
+{
+
 }
 
 //===================================================================
